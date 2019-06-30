d5927eec3efa868264ef5a79e3e9d9ac4b4ef1fe
debuginn <xianhui.meng.cn@gmail.com>
2019-06-30 18:21:04 +0800

    添加管理员功能及管理员列表展现及分页

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use Symfony\Component\Console\Helper\Table;
 
 /**
  * 管理员控制器
@@ -17,6 +18,53 @@ class AdminController extends Controller
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function index(){
-        return view("admin.admin.index");
+        $data = DB::table('dzushop_admin')->orderBy('id','asc')->paginate(10);
+        return view("admin.admin.index")->with('data',$data);
+    }
+
+    /**
+     * 管理员添加校验管理员是否存在
+     * @param Request $request
+     */
+    public function checkadmin(Request $request){
+
+    }
+    /**
+     * 管理员添加保存操作方法
+     * @param Request $request
+     */
+    public function store(Request $request){
+        // 接收前台传来的数值
+        $name = $request->input('name');
+        $pass = $request->input('pass');
+        $status = $request->input('status');
+        //判断传来的值是否合法
+        if($name == ""){
+            exit(json_encode(array('code'=>1, 'msg'=>'传入name参数异常，请联系管理员')));
+        }
+        if($pass == ""){
+            exit(json_encode(array('code'=>1, 'msg'=>'传入pass参数异常，请联系管理员')));
+        }
+        if($status == ""){
+            exit(json_encode(array('code'=>1, 'msg'=>'传入status参数异常，请联系管理员')));
+        }
+        //完善数据库字段
+        $md5Pass = md5($pass);
+        $time = time();
+
+        $result = DB::table('dzushop_admin')->insert([
+            'name' => $name,
+            'pass' => $md5Pass,
+            'time' => $time,
+            'lasttime' => $time,
+            'count' => 1,
+            'status' => $status
+        ]);
+
+        if($result){
+            exit(json_encode(array('code'=>0, 'msg'=>"新增管理员成功")));
+        }else{
+            exit(json_encode(array('code'=>1, 'msg'=>"新增数据出现异常")));
+        }
     }
 }
--- resources/views/admin/admin/index.blade.php
+++ resources/views/admin/admin/index.blade.php
@@ -39,38 +39,38 @@
                     <th>姓名</th>
                     <th>状态</th>
                     <th>创建时间</th>
-                    <th>更改时间</th>
+                    <th>最后登录时间</th>
+                    <th></th>
                 </tr>
                 </thead>
                 <tbody>
-{{--                @foreach($data as $value)--}}
-{{--                    <tr>--}}
-{{--                        --}}{{--按序号输出数值--}}
-{{--                        <td>{{ $value->id }}</td>--}}
-{{--                        <td>{{ $value->name }}</td>--}}
-{{--                        <td>--}}
-{{--                            @if($value->status == 0)--}}
-{{--                                <span class="btn btn-sm btn-rounded btn-gradient-success" onclick="status(this, {{ $value->id }})">开启</span>--}}
-{{--                            @elseif($value->status == 1)--}}
-{{--                                <span class="btn btn-sm btn-rounded btn-gradient-danger" onclick="status(this, {{ $value->id }})">禁用</span>--}}
-{{--                            @endif--}}
-{{--                        </td>--}}
-{{--                        <td>{{ $value->time }}</td>--}}
-{{--                        <td>{{ $value->settime }}</td>--}}
-{{--                        <td>--}}
-{{--                            <a class="btn btn-sm btn-gradient-success" href="/admin/user/edit/{{ $value->id }}">--}}
-{{--                                修改--}}
-{{--                            </a>--}}
-{{--                            <button class="btn btn-sm btn-gradient-danger" onclick="del(this, {{ $value->id }})">--}}
-{{--                                删除--}}
-{{--                            </button>--}}
-{{--                        </td>--}}
-{{--                    </tr>--}}
-{{--                @endforeach--}}
+                @foreach($data as $value)
+                    <tr>
+                        <td>{{ $value->id }}</td>
+                        <td>{{ $value->name }}</td>
+                        <td>
+                            @if($value->status == 0)
+                                <span class="btn btn-sm btn-rounded btn-gradient-success" onclick="changeStatus(this, {{ $value->id }})">开启</span>
+                            @elseif($value->status == 1)
+                                <span class="btn btn-sm btn-rounded btn-gradient-danger" onclick="changeStatus(this, {{ $value->id }})">禁用</span>
+                            @endif
+                        </td>
+                        <td>{{ date("Y-m-d H:i:s", $value->time) }}</td>
+                        <td>{{ date("Y-m-d H:i:s", $value->lasttime) }}</td>
+                        <td>
+                            <a class="btn btn-sm btn-gradient-success" href="/admin/admin/edit/{{ $value->id }}">
+                                修改
+                            </a>
+                            <button class="btn btn-sm btn-gradient-danger" onclick="del(this, {{ $value->id }})">
+                                删除
+                            </button>
+                        </td>
+                    </tr>
+                @endforeach
                 </tbody>
             </table>
             <div class="admin-page">
-{{--                {{ $data->links() }}--}}
+                {{ $data->links() }}
             </div>
         </div>
     </div>
@@ -84,31 +84,32 @@
                 </div>
                 <div class="modal-body">
                     <form onsubmit="return false;" id="formAddAdmin">
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
                         <div class="form-group">
                             <label for="">管理员名称</label>
-                            <input type="text" name="name" class="form-control" placeholder="请输入管理员名称" onchange="adminCheck();">
+                            <input type="text" name="name" class="form-control" placeholder="请输入管理员名称" onchange="adminCheck();" required>
                         </div>
                         <div class="form-group">
                             <label for="">密码</label>
-                            <input type="password" name="pass" class="form-control" placeholder="请输入新的密码">
+                            <input type="password" name="pass" class="form-control" placeholder="请输入新的密码" required>
                         </div>
                         <div class="form-group">
                             <label for="">重复密码</label>
-                            <input type="password" name="repass" class="form-control" placeholder="请再次输入新的密码" onchange="passCheck();">
+                            <input type="password" name="repass" class="form-control" placeholder="请再次输入新的密码" onchange="passCheck();" required>
                         </div>
                         <div class="form-group">
                             <label for="">状态</label>
                             <br>
                             <div class="form-check">
                                 <label class="form-check-label">
-                                    <input type="radio" class="form-check-input" name="userstatus" id="userstatus1" value="0" checked>
+                                    <input type="radio" class="form-check-input" name="status" id="userstatus1" value="0" checked>
                                     启用
                                 </label>
                             </div>
 
                             <div class="form-check">
                                 <label class="form-check-label">
-                                    <input type="radio" class="form-check-input" name="userstatus" id="userstatus2" value="1">
+                                    <input type="radio" class="form-check-input" name="status" id="userstatus2" value="1">
                                     禁用
                                 </label>
                             </div>
@@ -117,6 +118,7 @@
                         <div class="form-group">
                             <input type="submit" value="提交" class="btn btn-gradient-success" onclick="addAdmin();">
                             <input type="reset" value="重置" class="btn btn-gradient-danger">
+                            <button type="button" class="btn btn-gradient-primary" aria-label="Close" data-dismiss="modal">关闭当前窗口</button>
                         </div>
                     </form>
                 </div>
@@ -126,11 +128,73 @@
     {{--添加管理员模态框结束--}}
     <script type="text/javascript">
         /**
+         * 检查用户输入用户名是否存在
+         * 若存在，提示已经存在
+         * 若不存在，无提示
+         */
+        function adminCheck(){
+            var name = $.trim($('input[name="name"]').val());
+            //此处进行校验，第二个管理员开始校验
+        }
+        /**
+         *
+         */
+        function passCheck(){
+            var pass = $.trim($('input[name="pass"]').val());
+            var repass = $.trim($('input[name="repass"]').val());
+            if(pass != repass){
+                bootbox.alert("两次密码输入不正确，请重新输入");
+                // 清空密码及重复密码内容
+                $('input[name="pass"]').val("");
+                $('input[name="repass"]').val("");
+                // 定位到密码输入栏
+                $('input[name="pass"]').focus();
+            }
+        }
+        /**
          * 添加管理员操作
          */
         function addAdmin(){
-            str = $("#formAddAdmin").serialize();
-            bootbox.alert(str);
+            // 获取用户注册信息
+            var name = $.trim($('input[name="name"]').val());
+            var pass = $.trim($('input[name="pass"]').val());
+            var repass = $.trim($('input[name="repass"]').val());
+            var status = $('input[name="status"]').val();
+            //判断是否接收到值
+            if(name == ""){
+                bootbox.alert("管理员名称没有获取到");
+                $('input[name="name"]').focus();
+                return false;
+            }
+            if(pass == ""){
+                bootbox.alert("管理员密码没有获取到");
+                $('input[name="pass"]').focus();
+                return false;
+            }
+            if(repass == ""){
+                bootbox.alert("管理员重复密码没有获取到");
+                $('input[name="repass"]').focus();
+                return false;
+            }
+
+            $.post(
+                // 请求地址
+                '/admin/admin/store',
+                // 请求参数及csrf认证
+                $('#formAddAdmin').serialize(),
+                // 请求回调函数
+                function(res){
+                    if(res.code > 0){
+                        bootbox.alert(res.msg);
+                    }
+                    if(res.code == 0){
+                        bootbox.alert(res.msg);
+                    }
+                },
+                // 传输采用json
+                'json'
+            );
+
         }
     </script>
 @endsection
